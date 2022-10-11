99d029507c4d905cd211f4e4b046ca28fc10bd79
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-10-11 07:03:51 -0700

    add prototype with 3tiers

---
+++ Prototype-1/business/PromotionBLL.php
@@ -0,0 +1,29 @@
+<?php
+
+include ("Data_Acess/PromoManager.php");
+
+
+class PromoBLL{
+
+    private $PromoManager = null ;
+    public  function __construct()
+    {
+       $this->PromoManager = new PromoManager();
+    
+    }
+
+
+    function GetAllData(){
+
+       return $this->PromoManager->GetData();
+
+    }
+    function AddData($Promo){
+
+       return $this->PromoManager->AddData($Promo);
+
+    }
+
+}
+
+?>
